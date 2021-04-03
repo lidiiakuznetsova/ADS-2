081fbc089edb9c4e73495b10237ec60442ca7c1c
lidiiakuznetsova <76099410+lidiiakuznetsova@users.noreply.github.com>
2021-04-03 14:23:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 int countPairs1(int *arr, int len, int value) {
 int a = 0;
 for (int i = 0; i < len-1; i++) {
-for (int j = i+1; j <= len; j++) {
-if ((arr[i] + arr[j]) == value) a=a+1;
+for (int j = i+1; j < len; j++) {
+if ((arr[i] + arr[j]) == value) a++;
 }
 }
 return a;
@@ -19,7 +19,7 @@ else
 r = m;
 }
 len = r - 1;
-for (int i = l; i >= 0; i--) {
+for (int i = len; i >= 0; i--) {
 for (int j = 0; j < i; j++) {
 if (arr[i] + arr[j] == value)
 a++;
